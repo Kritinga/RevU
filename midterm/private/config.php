6ddda70eabd1f0feac13f565e5edfd29e5c37e32
Kritinga <johncasakit@gmail.com>
2024-10-14 10:48:13 +0800

    Delete midterm directory

--- midterm/private/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$localhost = "localhost";
-	$user = "root";
-	$pass = "";
-	$dbase = "ccs";
-
-	$con = mysqli_connect($localhost, $user, $pass, $dbase) or die("Sorry! You can't connect to the database");
-?>
--- midterm/public/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-		echo "<script>alert('Successfully Logged Out!')</script>";
-        echo "<script>window.location.href='LoginHere'</script>";
-?>
